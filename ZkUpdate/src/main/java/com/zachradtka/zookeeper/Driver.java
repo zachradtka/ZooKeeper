eb2951d65ba75e63e4677b237b23b4388279d2b7
Zachary Radtka <zachary.radtka@gmail.com>
2015-02-03 11:01:29 +0100

    ZER - Updated ZkUpdate to work from the command line

--- ZkUpdate/src/main/java/com/zachradtka/zookeeper/Driver.java
+++ ZkUpdate/src/main/java/com/zachradtka/zookeeper/Driver.java
@@ -1,10 +1,38 @@
 package com.zachradtka.zookeeper;
 
+import java.io.IOException;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static ZooKeeper zk;
 
+	private static ZkConnector zkc;
+
+	public static void update(String path, byte[] data) throws KeeperException,
+			InterruptedException {
+		zk.setData(path, data, zk.exists(path, true).getAversion());
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException,
+			KeeperException {
+
+		// Ensure the correct number of args was passed in
+		if (args.length != 4) {
+			System.err.println("USAGE: ZkConnector <hostname> <port> <znode> <data>");
+			System.exit(1);
+		}
+
+		String hostname = args[0] + ":" + args[1];
+		String path = args[2];
+		byte[] data = args[3].getBytes();
+
+		zkc = new ZkConnector();
+		zk = zkc.connect(hostname);
+
+		update(path, data);
 	}
 
 }
